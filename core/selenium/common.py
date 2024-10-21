68bda8a260f5f4856b774b4ffe9186a072feb08b
vicgrabru <vgravanbru@gmail.com>
2024-10-21 21:02:08 +0200

    Testing fix

--- core/selenium/common.py
+++ core/selenium/common.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,9 +7,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 def initialize_driver():
     # Initializes the browser options
     options = webdriver.ChromeOptions()
+    driver_path = ChromeDriverManager().install()
 
     # Initialise the browser using WebDriver Manager
-    service = Service(ChromeDriverManager().install())
+    chrome_driver_binary = os.path.join(os.path.dirname(driver_path), 'chromedriver')
+    service = Service(chrome_driver_binary)
+    
     driver = webdriver.Chrome(service=service, options=options)
     return driver
 
