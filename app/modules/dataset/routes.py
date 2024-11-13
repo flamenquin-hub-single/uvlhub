f79b412fc1ae0f2d31a74fa3b26fb7baec3cadd3
Raúl Montalbán Martín <raumonmar1@alum.us.es>
2024-11-13 17:11:48 +0100

    refactor(notepad): clean and organize routes in routes.py for improved readability

--- app/modules/dataset/routes.py
+++ app/modules/dataset/routes.py
@@ -6,24 +6,11 @@ import tempfile
 import uuid
 from datetime import datetime, timezone
 from zipfile import ZipFile
-from flask import jsonify, redirect, url_for
-
-from flask import (
-    redirect,
-    render_template,
-    request,
-    jsonify,
-    send_from_directory,
-    make_response,
-    abort,
-    url_for,
-)
+from flask import jsonify, redirect, url_for, render_template, request, send_from_directory, make_response, abort
 from flask_login import login_required, current_user
 
 from app.modules.dataset.forms import DataSetForm
-from app.modules.dataset.models import (
-    DSDownloadRecord
-)
+from app.modules.dataset.models import DSDownloadRecord
 from app.modules.dataset import dataset_bp
 from app.modules.dataset.services import (
     AuthorService,
@@ -37,7 +24,6 @@ from app.modules.zenodo.services import ZenodoService
 
 logger = logging.getLogger(__name__)
 
-
 dataset_service = DataSetService()
 author_service = AuthorService()
 dsmetadata_service = DSMetaDataService()
@@ -45,13 +31,11 @@ zenodo_service = ZenodoService()
 doi_mapping_service = DOIMappingService()
 ds_view_record_service = DSViewRecordService()
 
-
 @dataset_bp.route("/dataset/upload", methods=["GET", "POST"])
 @login_required
 def create_dataset():
     form = DataSetForm()
     if request.method == "POST":
-
         dataset = None
 
         if not form.validate_on_submit():
@@ -63,8 +47,8 @@ def create_dataset():
             logger.info(f"Created dataset: {dataset}")
             dataset_service.move_feature_models(dataset)
         except Exception as exc:
-            logger.exception(f"Exception while create dataset data in local {exc}")
-            return jsonify({"Exception while create dataset data in local: ": str(exc)}), 400
+            logger.exception(f"Exception while creating dataset data in local {exc}")
+            return jsonify({"Exception while creating dataset data in local": str(exc)}), 400
 
         # send dataset as deposition to Zenodo
         data = {}
@@ -75,30 +59,22 @@ def create_dataset():
         except Exception as exc:
             data = {}
             zenodo_response_json = {}
-            logger.exception(f"Exception while create dataset data in Zenodo {exc}")
+            logger.exception(f"Exception while creating dataset data in Zenodo {exc}")
 
         if data.get("conceptrecid"):
             deposition_id = data.get("id")
-
-            # update dataset with deposition id in Zenodo
             dataset_service.update_dsmetadata(dataset.ds_meta_data_id, deposition_id=deposition_id)
 
             try:
-                # iterate for each feature model (one feature model = one request to Zenodo)
                 for feature_model in dataset.feature_models:
                     zenodo_service.upload_file(dataset, deposition_id, feature_model)
-
-                # publish deposition
                 zenodo_service.publish_deposition(deposition_id)
-
-                # update DOI
                 deposition_doi = zenodo_service.get_doi(deposition_id)
                 dataset_service.update_dsmetadata(dataset.ds_meta_data_id, dataset_doi=deposition_doi)
             except Exception as e:
-                msg = f"it has not been possible upload feature models in Zenodo and update the DOI: {e}"
+                msg = f"it has not been possible to upload feature models in Zenodo and update the DOI: {e}"
                 return jsonify({"message": msg}), 200
 
-        # Delete temp folder
         file_path = current_user.temp_folder()
         if os.path.exists(file_path) and os.path.isdir(file_path):
             shutil.rmtree(file_path)
@@ -107,7 +83,6 @@ def create_dataset():
         return jsonify({"message": msg}), 200
 
     return render_template("dataset/upload_dataset.html", form=form)
-
 
 @dataset_bp.route("/dataset/list", methods=["GET", "POST"])
 @login_required
@@ -118,7 +93,6 @@ def list_dataset():
         local_datasets=dataset_service.get_unsynchronized(current_user.id),
     )
 
-
 @dataset_bp.route("/dataset/file/upload", methods=["POST"])
 @login_required
 def upload():
@@ -128,14 +102,12 @@ def upload():
     if not file or not file.filename.endswith(".uvl"):
         return jsonify({"message": "No valid file"}), 400
 
-    # create temp folder
     if not os.path.exists(temp_folder):
         os.makedirs(temp_folder)
 
     file_path = os.path.join(temp_folder, file.filename)
 
     if os.path.exists(file_path):
-        # Generate unique filename (by recursion)
         base_name, extension = os.path.splitext(file.filename)
         i = 1
         while os.path.exists(
@@ -162,7 +134,6 @@ def upload():
         200,
     )
 
-
 @dataset_bp.route("/dataset/file/delete", methods=["POST"])
 def delete():
     data = request.get_json()
@@ -176,13 +147,10 @@ def delete():
 
     return jsonify({"error": "Error: File not found"})
 
-
 @dataset_bp.route("/dataset/download/<int:dataset_id>", methods=["GET"])
 def download_dataset(dataset_id):
     dataset = dataset_service.get_or_404(dataset_id)
-
     file_path = f"uploads/user_{dataset.user_id}/dataset_{dataset.id}/"
-
     temp_dir = tempfile.mkdtemp()
     zip_path = os.path.join(temp_dir, f"dataset_{dataset_id}.zip")
 
@@ -190,9 +158,7 @@ def download_dataset(dataset_id):
         for subdir, dirs, files in os.walk(file_path):
             for file in files:
                 full_path = os.path.join(subdir, file)
-
                 relative_path = os.path.relpath(full_path, file_path)
-
                 zipf.write(
                     full_path,
                     arcname=os.path.join(
@@ -202,10 +168,7 @@ def download_dataset(dataset_id):
 
     user_cookie = request.cookies.get("download_cookie")
     if not user_cookie:
-        user_cookie = str(
-            uuid.uuid4()
-        )  # Generate a new unique identifier if it does not exist
-        # Save the cookie to the user's browser
+        user_cookie = str(uuid.uuid4())
         resp = make_response(
             send_from_directory(
                 temp_dir,
@@ -223,7 +186,6 @@ def download_dataset(dataset_id):
             mimetype="application/zip",
         )
 
-    # Check if the download record already exists for this cookie
     existing_record = DSDownloadRecord.query.filter_by(
         user_id=current_user.id if current_user.is_authenticated else None,
         dataset_id=dataset_id,
@@ -231,7 +193,6 @@ def download_dataset(dataset_id):
     ).first()
 
     if not existing_record:
-        # Record the download in your database
         DSDownloadRecordService().create(
             user_id=current_user.id if current_user.is_authenticated else None,
             dataset_id=dataset_id,
@@ -241,38 +202,27 @@ def download_dataset(dataset_id):
 
     return resp
 
-
 @dataset_bp.route("/doi/<path:doi>/", methods=["GET"])
 def subdomain_index(doi):
-
-    # Check if the DOI is an old DOI
     new_doi = doi_mapping_service.get_new_doi(doi)
     if new_doi:
-        # Redirect to the same path with the new DOI
         return redirect(url_for('dataset.subdomain_index', doi=new_doi), code=302)
 
-    # Try to search the dataset by the provided DOI (which should already be the new one)
     ds_meta_data = dsmetadata_service.filter_by_doi(doi)
 
     if not ds_meta_data:
         abort(404)
 
-    # Get dataset
     dataset = ds_meta_data.data_set
-
-    # Save the cookie to the user's browser
     user_cookie = ds_view_record_service.create_cookie(dataset=dataset)
     resp = make_response(render_template("dataset/view_dataset.html", dataset=dataset))
     resp.set_cookie("view_cookie", user_cookie)
 
     return resp
 
-
 @dataset_bp.route("/dataset/unsynchronized/<int:dataset_id>/", methods=["GET"])
 @login_required
 def get_unsynchronized_dataset(dataset_id):
-
-    # Get dataset
     dataset = dataset_service.get_unsynchronized_dataset(current_user.id, dataset_id)
 
     if not dataset:
@@ -280,14 +230,11 @@ def get_unsynchronized_dataset(dataset_id):
 
     return render_template("dataset/view_dataset.html", dataset=dataset)
 
-
 @dataset_bp.route("/dataset/download/<int:file_id>/<string:format>", methods=["GET"])
 def download_dataset_format(file_id, format):
-    """
-    Endpoint to download dataset in the specified format.
+    """Endpoint to download dataset in the specified format.
     Formats supported: uvl, glencoe, splot, dimacs, zip.
     """
-    # Redirigir a los endpoints de flamapy según el formato solicitado
     if format == "glencoe":
         return redirect(url_for("flamapy.to_glencoe", file_id=file_id))
     elif format == "splot":
@@ -295,7 +242,6 @@ def download_dataset_format(file_id, format):
     elif format == "dimacs":
         return redirect(url_for("flamapy.to_cnf", file_id=file_id))
     elif format == "zip":
-        # Redirige al endpoint original de descarga ZIP
         return redirect(url_for("dataset.download_dataset", dataset_id=file_id))
     else:
         return jsonify({"error": "Formato no soportado"}), 400
