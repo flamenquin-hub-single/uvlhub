5a0afa8905f348a607b5f751df02a9316749d64e
JoseGTD <josgardel8@alum.us.es>
2024-12-15 02:36:18 +0100

    feat(zenodo): basic fakenodo layout, api connection still failing

--- app/modules/zenodo/services.py
+++ app/modules/zenodo/services.py
@@ -27,11 +27,11 @@ class ZenodoService(BaseService):
         ZENODO_API_URL = ""
 
         if FLASK_ENV == "development":
-            ZENODO_API_URL = os.getenv("ZENODO_API_URL", "https://sandbox.zenodo.org/api/deposit/depositions")
+            ZENODO_API_URL = os.getenv("ZENODO_API_URL", "https://localhost:5001/api/fakenodo/depositions")
         elif FLASK_ENV == "production":
             ZENODO_API_URL = os.getenv("ZENODO_API_URL", "https://zenodo.org/api/deposit/depositions")
         else:
-            ZENODO_API_URL = os.getenv("ZENODO_API_URL", "https://sandbox.zenodo.org/api/deposit/depositions")
+            ZENODO_API_URL = os.getenv("ZENODO_API_URL", "https://localhost:5001/api/fakenodo/depositions")
 
         return ZENODO_API_URL
 
