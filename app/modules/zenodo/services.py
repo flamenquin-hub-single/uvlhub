d6e9198db88472fed91bf40633cf3000218e828d
JoseGTD <josgardel8@alum.us.es>
2024-12-15 18:57:51 +0100

    fix(zenodo): changed enviroment variable loading to work when fakenodo is deployed

--- app/modules/zenodo/services.py
+++ app/modules/zenodo/services.py
@@ -27,11 +27,11 @@ class ZenodoService(BaseService):
         ZENODO_API_URL = ""
 
         if FLASK_ENV == "development":
-            ZENODO_API_URL = os.getenv("ZENODO_API_URL", "http://localhost:5001/api/fakenodo/depositions")
+            ZENODO_API_URL = os.getenv("FAKENODO_API_URL", "http://localhost:5001/api/fakenodo/depositions")
         elif FLASK_ENV == "production":
             ZENODO_API_URL = os.getenv("ZENODO_API_URL", "https://zenodo.org/api/deposit/depositions")
         else:
-            ZENODO_API_URL = os.getenv("ZENODO_API_URL", "http://localhost:5001/api/fakenodo/depositions")
+            ZENODO_API_URL = os.getenv("FAKENODO_API_URL", "http://localhost:5001/api/fakenodo/depositions")
 
         return ZENODO_API_URL
 
