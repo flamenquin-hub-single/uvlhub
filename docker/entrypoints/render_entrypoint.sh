4284d1110daa29ece1fd6e027b50fe1364bf611b
vicgrabru <vgravanbru@gmail.com>
2024-12-17 19:53:52 +0100

    fix(fakenodo): possible fix for fakenodo deploy

--- docker/entrypoints/render_entrypoint.sh
+++ docker/entrypoints/render_entrypoint.sh
@@ -51,7 +51,7 @@ else
     flask db upgrade
 fi
 
-python3 fakenodo/run.py &
+python3 -m fakenodo/run.py &
 
 # Start the application using Gunicorn, binding it to port 80
 # Set the logging level to info and the timeout to 3600 seconds
